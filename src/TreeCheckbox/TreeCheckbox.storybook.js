2cd1451d31588ab5accfdb575c640c0ee8897196
Bruno Agutoli <bruno.agutoli@gmail.com>
2018-12-22 19:00:41 -0200

    created readme

--- src/TreeCheckbox/TreeCheckbox.storybook.js
+++ src/TreeCheckbox/TreeCheckbox.storybook.js
@@ -29,8 +29,6 @@ const nodes = [
   ]),
 ]
 
-console.log(JSON.stringify(nodes, null, 2))
-
 storiesOf('Molecule/TreeCheckbox', module)
   .add('showToggleText=true', () => (
     <div style={{margin: '20px', width: '100%'}}>
